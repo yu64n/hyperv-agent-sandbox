8d8a5b12719c28cf60b5519dae627ee41ae07b6a
Yu <shooting.star4242@gmail.com>
2026-08-02 13:26:26 +0900

    fix: update Nix development shell inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Tools for the Hyper-V agent sandbox";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }: let system = "x86_64-linux"; pkgs = import nixpkgs { inherit system; }; in {
-    devShells.${system}.default = pkgs.mkShell { packages = with pkgs; [ ansible-core git gnumake jq openssh shellcheck shfmt yamllint yq-go xorriso ]; };
+    devShells.${system}.default = pkgs.mkShell { packages = with pkgs; [ ansible git gnumake jq openssh shellcheck shfmt yamllint yq-go xorriso ]; };
   };
 }
-
